d7657a2befc5d74cf221722c1e98a791718dda75
egibs <theg859@gmail.com>
2015-05-05 17:47:05 -0500

    webview segue/back button functional

--- NewsExplorer/NewsViewController.swift
+++ NewsExplorer/NewsViewController.swift
@@ -225,14 +225,16 @@ class NewsViewController: UIViewController, CLLocationManagerDelegate, UITableVi
         }
     }
     
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    override func prepareForSegue(segue: UIStoryboardSegue,
+        sender: AnyObject?) {
+            
+            if segue.identifier == "showWebView" {
+                let detailViewController = segue.destinationViewController
+                    as NewsWebViewController
+                detailViewController.webSite = (string: url)
+        }
     }
-    */
+    
+    
 
 }
